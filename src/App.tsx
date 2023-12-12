7d1c1f9832e4e7a2873aea07f08a49ef636bc9ff
lukebrider-woolard <lukebrider-woolard@outlook.com>
2023-12-12 17:39:15 +0000

    test first md file

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
+import { useEffect, useState } from "react";
+import Markdown from "react-markdown";
+import notes from "../lore/D&D/Planning/Ideas and Notes.md";
 import "./App.css";
 
 function App() {
+  const [testText, setTestText] = useState("");
+
+  useEffect(() => {
+    fetch(notes)
+      .then((res) => res.text())
+      .then((text) => setTestText(text));
+  }, []);
+
   return (
     <>
       <h1>Welcome to the Encyclopaedia of the Known World</h1>
+      <div className="content">
+        <Markdown>{testText}</Markdown>
+      </div>
       <p>The Known World© - Luke Brider-Woolard 2013</p>
     </>
   );
