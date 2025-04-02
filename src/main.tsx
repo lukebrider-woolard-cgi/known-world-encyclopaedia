d31db1c4afc483722f7348a55bdbc2a286f8f7b3
lukebrider-woolard <lukebrider-woolard@outlook.com>
2025-04-02 22:40:55 +0100

    fix(app): fix routing

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='known-world-encyclopaedia' element={<App />}>
+        <Route path='/' element={<App />}>
           <Route index element={<WelcomePage />} />
           <Route path='articles/category/:categoryId/article/:articleId' element={<ArticlesPage />} />
           <Route path='maps' element={<MapsPage />} />
